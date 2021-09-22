d51f689d2c79b60b7e2291028ee337eca5e9cc5e
Michael Fisher <mifi@hey.com>
2021-09-22 19:05:57 +0300

    Scanner skeleton and main func

--- src/main.rs
+++ src/main.rs
@@ -1,2 +1,85 @@
+use std::{env, fs, io, process, fmt};
+
 fn main() {
+    let args = env::args();
+    if args.len() > 2 {
+        println!("Usage: lox [script]");
+    } else if args.len() == 2 {
+        let file = args.skip(1).next().unwrap();
+        run_file(file);
+    } else {
+        run_prompt();
+    }
+}
+
+fn run_file(file: String) {
+    let text  = fs::read_to_string(file).expect("file read failed");
+    run(text.as_str());
+    unsafe {
+        if HAD_ERROR {
+            process::exit(65);
+        }
+    }
+}
+
+fn run_prompt() {
+    let stdin = io::stdin();
+    let mut input = String::new();
+    loop {
+        print!("> ");
+ 
+        let bytes_read = stdin.read_line(&mut input).expect("read line failed");
+        let eof = bytes_read == 0;
+        if eof {
+            break;
+        }
+
+        run(input.as_ref());  
+
+        input.clear();
+        unsafe {
+            HAD_ERROR = false;
+        }
+    }
+}
+
+fn run(source: &str) {
+    let scanner = Scanner::new(source);
+    let tokens = scanner.tokens();
+    for token in tokens {
+        println!("{}", token);
+    }
+}
+
+fn error(line: usize, message: &str) {
+    report(line, "", message);
+}
+
+fn report(line: usize, place: &str, message: &str) {
+    eprintln!("[line {}] Error{}: {}", line, place, message);
+    unsafe {
+        HAD_ERROR = true;
+    }
+}
+
+static mut HAD_ERROR: bool = false;
+
+struct Scanner{}
+
+impl Scanner {
+    fn new(source: &str) -> Self {
+        todo!();
+    }
+
+    fn tokens(&self) -> Vec<Token> {
+        todo!();
+    }
+}
+
+enum Token {}
+
+impl fmt::Display for Token {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        todo!();
+    }
 }
