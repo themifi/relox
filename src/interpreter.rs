760b8b080371984d9240e824cd2ab326722a1bd0
Michael Fisher <mifi@hey.com>
2021-10-19 14:14:58 +0300

    Interpreter tests

--- src/interpreter.rs
+++ src/interpreter.rs
@@ -151,3 +151,408 @@ fn check_number_operands(
         })
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    fn interpret(expr: &dyn Expression) -> Result {
+        let interpreter = Interpreter::new();
+        interpreter.interpret(expr)
+    }
+
+    #[test]
+    fn interpret_literal() {
+        let literals = vec![
+            (TokenLiteral::Nil, Value::Nil),
+            (TokenLiteral::Boolean(true), Value::Boolean(true)),
+            (TokenLiteral::Number(4.0), Value::Number(4.0)),
+            (
+                TokenLiteral::String("foo".to_owned()),
+                Value::String("foo".to_owned()),
+            ),
+        ];
+
+        for (literal, value) in literals {
+            let expr = Literal { value: literal };
+            assert_eq!(Ok(value), interpret(&expr));
+        }
+    }
+
+    #[test]
+    fn interpret_number_negation() {
+        let expr = Unary {
+            operator: Token {
+                t: TokenType::Minus,
+                line: 1,
+                lexeme: "-".to_owned(),
+                literal: None,
+            },
+            right: Box::new(Literal {
+                value: TokenLiteral::Number(2.0),
+            }),
+        };
+        assert_eq!(Ok(Value::Number(-2.0)), interpret(&expr));
+    }
+
+    #[test]
+    fn interpret_bool_negation() {
+        let expr = Unary {
+            operator: Token {
+                t: TokenType::Bang,
+                line: 1,
+                lexeme: "!".to_owned(),
+                literal: None,
+            },
+            right: Box::new(Literal {
+                value: TokenLiteral::Boolean(true),
+            }),
+        };
+        assert_eq!(Ok(Value::Boolean(false)), interpret(&expr));
+    }
+
+    #[test]
+    fn interpret_negation_invalid_type() {
+        let literals = vec![
+            TokenLiteral::Nil,
+            TokenLiteral::String("foo".to_owned()),
+            TokenLiteral::Boolean(true),
+        ];
+        for literal in literals {
+            let expr = Unary {
+                operator: Token {
+                    t: TokenType::Minus,
+                    line: 1,
+                    lexeme: String::new(),
+                    literal: None,
+                },
+                right: Box::new(Literal { value: literal }),
+            };
+            assert_eq!(
+                Err(RuntimeError::OperandMustBeANumber {
+                    token: expr.operator.clone(),
+                }),
+                interpret(&expr)
+            );
+        }
+    }
+
+    #[test]
+    fn interpret_bang() {
+        let literals = vec![
+            (TokenLiteral::Nil, true),
+            (TokenLiteral::String("foo".to_owned()), false),
+            (TokenLiteral::Number(2.0), false),
+            (TokenLiteral::Boolean(true), false),
+            (TokenLiteral::Boolean(false), true),
+        ];
+        for (literal, result) in literals {
+            let expr = Unary {
+                operator: Token {
+                    t: TokenType::Bang,
+                    line: 1,
+                    lexeme: String::new(),
+                    literal: None,
+                },
+                right: Box::new(Literal { value: literal }),
+            };
+            assert_eq!(Ok(Value::Boolean(result)), interpret(&expr));
+        }
+    }
+
+    #[test]
+    fn interpret_grouping() {
+        let expr = Grouping {
+            expr: Box::new(Unary {
+                operator: Token {
+                    t: TokenType::Bang,
+                    line: 1,
+                    lexeme: String::new(),
+                    literal: None,
+                },
+                right: Box::new(Literal {
+                    value: TokenLiteral::Boolean(true),
+                }),
+            }),
+        };
+        assert_eq!(Ok(Value::Boolean(false)), interpret(&expr));
+    }
+
+    #[test]
+    fn interpret_numbers_operations() {
+        let data = vec![
+            (TokenType::Plus, 20.0),
+            (TokenType::Minus, 10.0),
+            (TokenType::Star, 75.0),
+            (TokenType::Slash, 3.0),
+        ];
+
+        for (token_type, result) in data {
+            let expr = Binary {
+                left: Box::new(Literal {
+                    value: TokenLiteral::Number(15.0),
+                }),
+                operator: Token {
+                    t: token_type,
+                    line: 1,
+                    lexeme: String::new(),
+                    literal: None,
+                },
+                right: Box::new(Literal {
+                    value: TokenLiteral::Number(5.0),
+                }),
+            };
+            assert_eq!(Ok(Value::Number(result)), interpret(&expr));
+        }
+    }
+
+    #[test]
+    fn interpret_numbers_operations_with_invalid_operand() {
+        let data = vec![
+            TokenType::Minus,
+            TokenType::Star,
+            TokenType::Slash,
+            TokenType::Greater,
+            TokenType::GreaterEqual,
+            TokenType::Less,
+            TokenType::LessEqual,
+        ];
+
+        for token_type in data {
+            let operands = vec![
+                (TokenLiteral::Number(15.0), TokenLiteral::Nil),
+                (
+                    TokenLiteral::Number(15.0),
+                    TokenLiteral::String("foo".to_owned()),
+                ),
+                (TokenLiteral::Number(15.0), TokenLiteral::Boolean(true)),
+            ];
+
+            for (left, right) in operands {
+                let expr = Binary {
+                    left: Box::new(Literal { value: left }),
+                    operator: Token {
+                        t: token_type,
+                        line: 1,
+                        lexeme: String::new(),
+                        literal: None,
+                    },
+                    right: Box::new(Literal { value: right }),
+                };
+                assert_eq!(
+                    Err(RuntimeError::OperandsMustBeNumbers {
+                        token: expr.operator.clone()
+                    }),
+                    interpret(&expr)
+                );
+            }
+        }
+    }
+
+    #[test]
+    fn interpret_addition_with_invalid_operand() {
+        let operands = vec![
+            // number with others
+            (TokenLiteral::Number(15.0), TokenLiteral::Nil),
+            (TokenLiteral::Number(15.0), TokenLiteral::Boolean(true)),
+            (
+                TokenLiteral::Number(15.0),
+                TokenLiteral::String("foo".to_owned()),
+            ),
+            // string with others
+            (
+                TokenLiteral::String("foo".to_owned()),
+                TokenLiteral::Boolean(true),
+            ),
+            (TokenLiteral::String("foo".to_owned()), TokenLiteral::Nil),
+            (
+                TokenLiteral::String("foo".to_owned()),
+                TokenLiteral::Number(2.0),
+            ),
+        ];
+
+        for (left, right) in operands {
+            let expr = Binary {
+                left: Box::new(Literal { value: left }),
+                operator: Token {
+                    t: TokenType::Plus,
+                    line: 1,
+                    lexeme: String::new(),
+                    literal: None,
+                },
+                right: Box::new(Literal { value: right }),
+            };
+            assert_eq!(
+                Err(RuntimeError::OperandsMustBeTwoNumbersOrTwoStrings {
+                    token: expr.operator.clone()
+                }),
+                interpret(&expr)
+            );
+        }
+    }
+
+    #[test]
+    fn interpret_numbers_comparsion() {
+        let data = vec![
+            (TokenType::Greater, 2.0, 3.0, false),
+            (TokenType::Greater, 3.0, 3.0, false),
+            (TokenType::Greater, 4.0, 3.0, true),
+            (TokenType::GreaterEqual, 2.0, 3.0, false),
+            (TokenType::GreaterEqual, 3.0, 3.0, true),
+            (TokenType::GreaterEqual, 4.0, 3.0, true),
+            (TokenType::Less, 2.0, 3.0, true),
+            (TokenType::Less, 3.0, 3.0, false),
+            (TokenType::Less, 4.0, 3.0, false),
+            (TokenType::LessEqual, 2.0, 3.0, true),
+            (TokenType::LessEqual, 3.0, 3.0, true),
+            (TokenType::LessEqual, 4.0, 3.0, false),
+            (TokenType::EqualEqual, 2.0, 3.0, false),
+            (TokenType::EqualEqual, 3.0, 3.0, true),
+            (TokenType::EqualEqual, 4.0, 3.0, false),
+            (TokenType::BangEqual, 2.0, 3.0, true),
+            (TokenType::BangEqual, 3.0, 3.0, false),
+            (TokenType::BangEqual, 4.0, 3.0, true),
+        ];
+
+        for (token_type, left, right, result) in data {
+            let expr = Binary {
+                left: Box::new(Literal {
+                    value: TokenLiteral::Number(left),
+                }),
+                operator: Token {
+                    t: token_type,
+                    line: 1,
+                    lexeme: String::new(),
+                    literal: None,
+                },
+                right: Box::new(Literal {
+                    value: TokenLiteral::Number(right),
+                }),
+            };
+            assert_eq!(Ok(Value::Boolean(result)), interpret(&expr));
+        }
+    }
+
+    #[test]
+    fn interpret_strings_addition() {
+        let expr = Binary {
+            left: Box::new(Literal {
+                value: TokenLiteral::String("foo".to_owned()),
+            }),
+            operator: Token {
+                t: TokenType::Plus,
+                line: 1,
+                lexeme: "+".to_owned(),
+                literal: None,
+            },
+            right: Box::new(Literal {
+                value: TokenLiteral::String("bar".to_owned()),
+            }),
+        };
+        assert_eq!(Ok(Value::String("foobar".to_owned())), interpret(&expr));
+    }
+
+    #[test]
+    fn interpret_literal_equality() {
+        let data = vec![
+            // nil with others
+            (TokenLiteral::Nil, TokenLiteral::Nil, true),
+            (TokenLiteral::Nil, TokenLiteral::Boolean(true), false),
+            (TokenLiteral::Nil, TokenLiteral::Number(2.0), false),
+            (
+                TokenLiteral::Nil,
+                TokenLiteral::String("foo".to_owned()),
+                false,
+            ),
+            // number with others
+            (TokenLiteral::Number(2.0), TokenLiteral::Number(2.0), true),
+            (TokenLiteral::Number(3.0), TokenLiteral::Number(2.0), false),
+            (TokenLiteral::Number(2.0), TokenLiteral::Nil, false),
+            (
+                TokenLiteral::Number(2.0),
+                TokenLiteral::Boolean(false),
+                false,
+            ),
+            (
+                TokenLiteral::Number(2.0),
+                TokenLiteral::String("foo".to_owned()),
+                false,
+            ),
+            (
+                TokenLiteral::Boolean(true),
+                TokenLiteral::Boolean(true),
+                true,
+            ),
+            (
+                TokenLiteral::Number(3.0),
+                TokenLiteral::Boolean(true),
+                false,
+            ),
+            // boolean with others
+            (
+                TokenLiteral::Boolean(true),
+                TokenLiteral::Boolean(true),
+                true,
+            ),
+            (
+                TokenLiteral::Boolean(true),
+                TokenLiteral::Boolean(false),
+                false,
+            ),
+            (TokenLiteral::Boolean(true), TokenLiteral::Nil, false),
+            (
+                TokenLiteral::Boolean(true),
+                TokenLiteral::Number(2.0),
+                false,
+            ),
+            (
+                TokenLiteral::Boolean(true),
+                TokenLiteral::String("foo".to_owned()),
+                false,
+            ),
+            // string with others
+            (
+                TokenLiteral::String("foo".to_owned()),
+                TokenLiteral::String("foo".to_owned()),
+                true,
+            ),
+            (
+                TokenLiteral::String("foo".to_owned()),
+                TokenLiteral::String("bar".to_owned()),
+                false,
+            ),
+            (
+                TokenLiteral::String("foo".to_owned()),
+                TokenLiteral::Nil,
+                false,
+            ),
+            (
+                TokenLiteral::String("foo".to_owned()),
+                TokenLiteral::Boolean(true),
+                false,
+            ),
+            (
+                TokenLiteral::String("foo".to_owned()),
+                TokenLiteral::Number(2.0),
+                false,
+            ),
+        ];
+
+        for (left, right, true_result) in data {
+            let mut expr = Binary {
+                left: Box::new(Literal { value: left }),
+                operator: Token {
+                    t: TokenType::EqualEqual,
+                    line: 1,
+                    lexeme: String::new(),
+                    literal: None,
+                },
+                right: Box::new(Literal { value: right }),
+            };
+            assert_eq!(Ok(Value::Boolean(true_result)), interpret(&expr));
+
+            expr.operator.t = TokenType::BangEqual;
+            assert_eq!(Ok(Value::Boolean(!true_result)), interpret(&expr));
+        }
+    }
+}
