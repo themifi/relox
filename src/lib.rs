47a97e078a9908edfb6d4a540463d0ecefe6a00d
Michael Fisher <mifi@hey.com>
2023-04-11 15:38:39 +0300

    Compile as library

---
+++ src/lib.rs
@@ -0,0 +1,58 @@
+use std::{
+    fs,
+    io::{self, Write},
+    process,
+};
+
+mod error;
+mod expression;
+mod interpreter;
+mod lox;
+mod parser;
+mod scanner;
+mod token;
+mod value;
+
+pub fn run_file(file: String) {
+    let text = fs::read_to_string(file).expect("file read failed");
+    run(text);
+    unsafe {
+        if error::HAD_ERROR {
+            process::exit(65);
+        } else if error::HAD_RUNTIME_ERROR {
+            process::exit(70);
+        }
+    }
+}
+
+pub fn run_prompt() {
+    let stdin = io::stdin();
+    loop {
+        print!("> ");
+        io::stdout().flush().unwrap();
+
+        let mut input = String::new();
+        let bytes_read = stdin.read_line(&mut input).expect("read line failed");
+        let eof = bytes_read == 0;
+        if eof {
+            break;
+        }
+
+        run(input);
+
+        unsafe {
+            error::HAD_ERROR = false;
+        }
+    }
+}
+
+fn run(source: String) {
+    let lox = lox::Lox::new();
+    let result = lox.run(source);
+    if let Err(e) = result {
+        match e {
+            lox::Error::Runtime(e) => error::runtime_error(e),
+            _ => error::report(e),
+        }
+    }
+}
